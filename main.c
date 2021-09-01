a6cbc0f5a7233426d233891466f9c3d8630c8ad8
Lucas Kuhn <lucasalexkuhn@gmail.com>
2021-09-01 19:54:11 -0300

    adds main

--- main.c
+++ main.c
@@ -6,20 +6,29 @@
 #include <sys/types.h>
 int main()
 {
-	printf("BUFFER_SIZE=%d\n\n", BUFFER_SIZE);
+	printf("BUFFER_SIZE=%d", BUFFER_SIZE);
 
+	puts("\n\n --- Empty ---");
 	int fd_0 = open("empty", O_RDONLY);
-	printf("GNL->(%s)", get_next_line(fd_0));
-	printf("GNL->(%s)", get_next_line(fd_0));
-	close(fd_0);
+	printf("->%s", get_next_line(fd_0));
+	printf("->%s", get_next_line(fd_0));
+	printf("->%s", get_next_line(fd_0));
 
+
+	puts("\n\n --- Numbers ---");
 	int fd_1 = open("numbers", O_RDONLY);
-	printf("GNL: %s", get_next_line(fd_1));
-	printf("GNL: %s", get_next_line(fd_1));
-	close(fd_1);
+	printf("->%s", get_next_line(fd_1));
+	printf("->%s", get_next_line(fd_1));
+	printf("->%s", get_next_line(fd_1));
 
+	puts("\n\n --- Song ---");
 	int fd_2 = open("song", O_RDONLY);
-	printf("GNL: %s", get_next_line(fd_2));
-	printf("GNL: %s", get_next_line(fd_2));
+	printf("->%s", get_next_line(fd_2));
+	printf("->%s", get_next_line(fd_2));
+	printf("->%s", get_next_line(fd_2));
+	printf("->%s", get_next_line(fd_2));
+
+	close(fd_0);
+	close(fd_1);
 	close(fd_2);
 }
